70a8bbcb003c9678714237b04555d0470d1466cd
Laban-Maseeh <labansahotra48@gmail.com>
2024-12-25 07:43:28 -0800

    Update 5th Module.cpp

--- 5th Module.cpp
+++ 5th Module.cpp
@@ -29,3 +29,65 @@ void loadData(const string &filename, string data[MAX_STUDENTS][MAX_FRIENDS + 1]
 
     file.close();
 }
+int countCommonFriends(const string data[MAX_STUDENTS][MAX_FRIENDS + 1], int student1, int student2) {
+    int commonCount = 0;
+
+    for (int i = 1; i <= MAX_FRIENDS; i++) {
+        if (data[student1][i].empty()) continue;  
+
+        for (int j = 1; j <= MAX_FRIENDS; j++) {
+            if (data[student2][j].empty()) continue;  // Skip empty entries
+
+            if (data[student1][i] == data[student2][j]) {
+                commonCount++;
+                break; 
+            }
+        }
+    }
+
+    return commonCount;
+}
+
+void createMultiplicityGraph(const string data[MAX_STUDENTS][MAX_FRIENDS + 1], int rowCount, int graph[MAX_STUDENTS][MAX_STUDENTS]) {
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < rowCount; j++) {
+            if (i != j) {
+                graph[i][j] = countCommonFriends(data, i, j);
+            } else {
+                graph[i][j] = 0;
+            }
+        }
+    }
+}
+
+void displayGraph(const int graph[MAX_STUDENTS][MAX_STUDENTS], int rowCount, const string data[MAX_STUDENTS][MAX_FRIENDS + 1]) {
+    cout << "Multiplicity Graph (Weighted Edges):\n";
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < rowCount; j++) {
+            if (graph[i][j] > 0) {
+                cout << data[i][0] << " -> " << data[j][0]
+                     << " [Weight: " << graph[i][j] << "]\n";
+            }
+        }
+    }
+}
+
+int main() {
+    string filename = "data.csv";
+    string data[MAX_STUDENTS][MAX_FRIENDS + 1];  // 2D array to store data
+    int rowCount;
+    
+    loadData(filename, data, rowCount);
+
+    if (rowCount == 0) {
+        cout << "No data loaded. Exiting...\n";
+        return 0;
+    }
+
+    int graph[MAX_STUDENTS][MAX_STUDENTS];
+    createMultiplicityGraph(data, rowCount, graph);
+
+    displayGraph(graph, rowCount, data);
+
+    return 0;
+}
